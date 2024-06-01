404e380cec6e2d6503a9fb2be3aba98d89294c04
rafaelnapo <rafaelnv.prof@gmail.com>
2024-06-01 12:59:05 -0300

    projeto meio do caminho
    01/06/2024
    
    main.py ainda esta na versão antiga e terminando a funcao de preencher fprmulario (falta clicar no botao de aceitar cokkies para rodigo rodar limpo)

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Automation to search for houses to buy or rent
  
+ideia: fazer um for por cador bloco de informaçoes e selecionar por meio de info de pequenas diferenças
+
+30/05
+xpath a mao dos itens <li>
+
+qunatidade de quartos tem o id =  "quartos{numquartosmin}" CONSEGUII
+
+amanha resolver o problema dos elemntos clicaveis (botao de cookies entendi ==  class = "body2__Paragraph-xstna9-0") o problema é esse botao mesmo, nao o de notificacao
+
+## contexto gpt:
+preciso que o codigo espere ate o elemento da ultima linha de codigo apareça para que ele clique no botao de cookies e assim permita a funcao posterior roda
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+# ! options é para quando o selenium abre e fecha sozinho
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+servico = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=servico, options=options)
+driver.get("https://www.chavesnamao.com.br/imoveis/brasil/") 
+time.sleep(6)
+driver.find_element(By.CLASS_NAME, "body2__Paragraph-xstna9-0").click()
---
+++ main.py
@@ -0,0 +1,60 @@
+
+# todo  import padrao para abrir o navegador junto do keys
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# ! options é para quando o selenium abre e fecha sozinho
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+servico = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=servico, options=options)
+
+
+
+# todo declarar funcoes 
+def preencher_formulario():
+    '''
+    Função vai pagar as principais informações para busca do imovel ideal baseado nos criterios
+    #? Return vai  ser uma tupla entao para acessar os valore temos que descompactar
+
+    '''
+        
+    print(60* '-_-_')
+    print("Digite sim, nao, indiferente ou valores")
+    localizacao = input("Localização(bairro):").upper()
+    alugar_comprar = input("Alugar ou Comprar:").upper()
+    apt_casa_vilagio = input("Casa apt ou cond fechado:").upper()
+    qnt_quartos = input("Qnt quarto:").upper()
+    preco_max = input("Preço max:").upper()
+    garagem = input("Garagem:").upper()
+    m2 = input("m2 max:").upper()
+
+    return localizacao, alugar_comprar, apt_casa_vilagio, qnt_quartos, preco_max, garagem, m2 
+
+# localizacao, alugar_comprar, apt_casa_vilagio, qnt_quartos, preco_max, garagem, m2 = preencher_formulario()
+# print(apt_casa_vilagio)
+
+# todo Código/ chamar função
+localizacao, alugar_comprar, apt_casa_vilagio, qnt_quartos, preco_max, garagem, m2 = preencher_formulario
+
+
+driver.get("https://www.chavesnamao.com.br/imoveis/brasil/") #driver entrar no site chaves nao mao/ imoveis / brasil
+
+# ! class tipoImovel__Container-sc-5e16gv-0
+# localizacao
+driver.find_element(By.CLASS_NAME, "realInput").send_keys(localizacao)  # apt / casa sobrado / casa em condominio / kitnet studio / cobertura
+
+# percorrer a lista de Tipo de imovel com um loop for e nisso fazer comparações com base no text de cada elemento e seleciona-lo caso condiza com o deejado
+elementos = driver.find_elements(By.CLASS_NAME, "tipoImovel__Container-sc-5e16gv-0")
+for item in elementos:
+    if "Shopping" in item.text:
+        item.click()
+        break  
+
+driver.find_element()  # faixa de preço   DE:   Ate:
+driver.find_element()  # qnt banheiro
+driver.find_element()  # area util   Min:   Max:
